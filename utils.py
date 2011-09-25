d5e3950f1cb835c80e8eebf1642c924a5cb308ff
Saining Li <saining.li@gmail.com>
2011-09-24 21:16:39 -0400

    added function generate_keystream

--- utils.py
+++ utils.py
@@ -1,7 +1,21 @@
 import re
+import echonest
 
 def bin_from_float(num):
-	str_num=str(num).replace(".", "")
-	x=re.search(r"^0*(?P<lol>[0-9]*)", str_num)
-	lol=x.group("lol") if x.group("lol") else "0"
-	return bin(int(lol))
+	"""
+	Takes a float, strips floating point, leading zeroes, exponents and converts to binary.
+	"""
+	str_num = str(abs(num)).replace(".", "")
+	x = re.search(r"^0*(?P<lol>[0-9]*)", str_num)
+	lol = x.group("lol") if x.group("lol") else "0"
+	return bin(int(lol))[2:0]
+	
+def generate_keystream(catalog):
+	"""
+	Takes a catalog id and generates four bit sequences.
+	"""
+	seqs=echonest.get_catalog_data(catalog)
+	for seq in seqs:
+		seq=[bin_from_float(x**seqs.index(seq)) for x in seq]
+	streams=["".join(seq) for seq in seqs]
+	
